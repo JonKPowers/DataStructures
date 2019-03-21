03478d4d57293c1d669bc0650601a1ca16df954c
Jon Powers <Jon.K.Powers+git@gmail.com>
2019-03-21 07:03:20 -0400

    Changed moveDiscs() to track output internally rather than dumping it to screen.

--- Lab_2/lab2RecuriveTowers.java
+++ Lab_2/lab2RecuriveTowers.java
@@ -6,26 +6,35 @@ class lab2RecursiveTowers{
         // Error checking that number of towers is valid
         // Error checking that file name is valid
 
-        moveDiscs(35, "A", "B", "C");
+        String output = moveDiscs(4, "A", "B", "C");
+        System.out.println("Final output string:\n" + output);
 
     }
 
-    public static void moveDiscs(int numDiscs, String start, String temp, String end){
+    public static String moveDiscs(int numDiscs, String start, String temp, String end){
+        String output = "";
+        
         if (numDiscs == 1){
             // Base case of 1 disc--just move it to its destination
-            makeMove(numDiscs, start, end);
+            output += makeMove(numDiscs, start, end);
         }
         else {
             // Otherwise, move other discs out of the way, move the disc
             // to its destination, then move the other discs to their
             // final desination.
-            moveDiscs(numDiscs - 1, start, end, temp);
-            makeMove(numDiscs, start, end);
-            moveDiscs(numDiscs - 1, temp, start, end);
+
+            output += moveDiscs(numDiscs - 1, start, end, temp);
+            output += makeMove(numDiscs, start, end);
+            output += moveDiscs(numDiscs - 1, temp, start, end);
+            
+            // System.out.println("Output from moveDiscs at numDiscs=" + numDiscs + ": " + output);
         }
+         return output;
     }
 
-    public static void makeMove(int numDiscs, String start, String end){
-        System.out.println("Move disc " + numDiscs + " from peg " + start + " to peg " + end + ".");
+    public static String makeMove(int numDiscs, String start, String end){
+        String outputString = "Move disc " + numDiscs + " from peg " + start + " to peg " + end + ".\n";
+        System.out.println(outputString);
+        return outputString;
     }
 }
